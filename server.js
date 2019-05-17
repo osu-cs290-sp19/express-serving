8b0419bb3ff3a800cba7a1288eff3435bf7a585a
Rob Hess <robwhess@gmail.com>
2019-05-17 09:59:30 -0700

    Add code from 5/17 lecture.

--- server.js
+++ server.js
@@ -1,13 +1,18 @@
 var express = require('express');
 var app = express();
 
+var logger = require('./logger');
+
+app.use(logger);
+
+app.use(express.static('public'));
+
 app.get('/', function (req, res, next) {
-  var content = "<html>";
-  content += "<body>";
-  content += "<h1>Hello world!</h1>";
-  content += "</body>";
-  content += "</html>";
-  res.status(200).send(content);
+  res.status(200).sendFile(__dirname + '/public/index.html');
+});
+
+app.get('/people', function (req, res, next) {
+  res.status(200).sendFile(__dirname + '/public/people.html');
 });
 
 app.get('/cats', function (req, res, next) {
@@ -17,6 +22,25 @@ app.get('/cats', function (req, res, next) {
   content += "</body>";
   content += "</html>";
   res.status(200).send(content);
+});
+
+var availableNames = [
+  'luke',
+  'leia',
+  'rey',
+  'finn',
+  'r2d2'
+];
+app.get('/people/:name', function (req, res, next) {
+  console.log("== req.params:", req.params);
+  var name = req.params.name;
+  if (availableNames.indexOf(name) !== -1) {
+    var htmlFile = __dirname + '/public/people/' + name + '.html';
+    console.log("== sending file:", htmlFile);
+    res.status(200).sendFile(htmlFile);
+  } else {
+    next();
+  }
 });
 
 app.get('*', function (req, res, next) {
